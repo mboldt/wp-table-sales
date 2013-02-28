90cff11b6ece876e0b25d0a7f08526047cee7432
Michael William Boldt <mike.boldt@gmail.com>
2013-02-28 17:07:32 -0600

    Add file for initial tables data.

---
+++ init-tables.php
@@ -0,0 +1,175 @@
+<?php
+function get_tables() {
+  $tables = array (
+                   1 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   2 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   3 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   4 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   5 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   6 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   7 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   8 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   9 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   10 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   11 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   12 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   13 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   14 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   15 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   16 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   17 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   18 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   19 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   20 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => false,
+                              ),
+                   21 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   22 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   23 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   24 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   25 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   26 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   27 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   28 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   29 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   30 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   31 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   32 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   33 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   34 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   35 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   36 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   37 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   38 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   39 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   40 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   41 => array("total-seats" => 8,
+                              "available-seats" => 8, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+                   42 => array("total-seats" => 10,
+                              "available-seats" => 10, /* TODO: pull from somewhere. */
+                              "sold-individually" => true,
+                              ),
+
+                   );
+ return $tables;
+}
